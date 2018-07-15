c0058379534ae2e5825c412e7d1fd12fc476281b
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-07-14 21:47:11 -0600

    initial commit adds base files, functionality to print board

---
+++ lib/board.rb
@@ -0,0 +1,25 @@
+require './lib/space.rb'
+
+class Board
+  attr_reader :new_board
+
+  def initialize
+    @new_board = Array.new(6) { Array.new(7) {". "} }
+  end
+
+  def display_board
+    # print "*CONNECT FOUR*\n"
+    print "A B C D E F G\n"
+    print @new_board[0].join + "\n"
+    print @new_board[1].join + "\n"
+    print @new_board[2].join + "\n"
+    print @new_board[3].join + "\n"
+    print @new_board[4].join + "\n"
+    print @new_board[5].join + "\n"
+  end
+
+  
+end
+
+board = Board.new
+board.display_board
---
+++ test/board_test.rb
@@ -0,0 +1,26 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/board'
+require 'pry'
+
+class BoardTest < Minitest::Test
+
+  def test_it_exists
+    board = Board.new
+    assert_instance_of Board, board
+  end
+
+  def test_it_starts_with_new_board
+    board = Board.new
+    expected = [
+      [". ", ". ", ". ", ". ", ". ", ". ", ". "],
+      [". ", ". ", ". ", ". ", ". ", ". ", ". "],
+      [". ", ". ", ". ", ". ", ". ", ". ", ". "],
+      [". ", ". ", ". ", ". ", ". ", ". ", ". "],
+      [". ", ". ", ". ", ". ", ". ", ". ", ". "],
+      [". ", ". ", ". ", ". ", ". ", ". ", ". "],
+    ]
+    assert_equal expected, board.new_board
+  end
+
+end
