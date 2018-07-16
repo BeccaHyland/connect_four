6c19695f01c4bb85c76d67b6bfb5667f1880a37e
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-07-16 11:42:07 -0600

    adds checkers method to convert incoming user choice of column to index (with test)

--- lib/checkers.rb
+++ lib/checkers.rb
@@ -1,10 +1,20 @@
 class Checkers
 
-  def receive_checker(user_choice)
-    puts "TEMP:You got to the receive_checker with column #{user_choice}"
+  # def initialize
+  # end
+
+  def receive_checker(user_column_choice)
+    column_index = convert_to_index(user_column_choice)
+    #puts "You got to the receive_checker with column #{user_choice}"
     #the above will be an ARGUMENT RECEIVED FROM THE RUNNER
     #when the runner calls this method
     #this method: ASK THE COLUMN where to put the piece
+  end
+
+  def convert_to_index(column)
+    column_to_index =
+    {"A" => 0, "B" => 1, "C" => 2, "D" => 3, "E" => 4, "F" => 5, "G" => 6}
+    column_to_index[column]
   end
 
 end
