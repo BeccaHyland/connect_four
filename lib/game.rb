a1263503effb57d196811f58d83190f39912eed8
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-07-18 20:37:33 -0600

    breaks up beginning of old runner into methods

--- lib/game.rb
+++ lib/game.rb
@@ -4,32 +4,40 @@ require './lib/cpu_player'
 require 'pry'
 
 class Game
+  attr_reader :board,
+              :message,
+              :cpu_player
 
-  # def initialize
-  #   board = Board.new
-  #   message = Message.new
-  #   cpu_player = CpuPlayer.new
-  # end
+  def initialize
+    @board = Board.new
+    @message = Message.new
+    @cpu_player = CpuPlayer.new
+  end
   #
-  # def start_game
-  #   board.display_board
-  #   puts message.welcome_instructions
-  #   gets
-  # end
+  def start_game
+    board.display_board
+    puts message.welcome_instructions
+    gets
+    get_clean_input
+  end
   #
-  # while board.board_full? == false
-  #   puts message.request_user_turn
-  #
-  #   user_column_choice = gets.strip.upcase
-  #   if user_column_choice == "QUIT"
-  #     break
-  #   else
-  #     good_input = ["A", "B", "C", "D", "E", "F", "G"]
-  #     until good_input.any? {|good| good == user_column_choice}
-  #       puts message.clarify
-  #       user_column_choice = gets.strip.upcase
-  #     end
-  #
+  def get_clean_input
+    #while board.board_full? == false
+    puts message.request_user_turn
+    user_column_choice = gets.strip.upcase
+    if user_column_choice == "QUIT"
+      break
+    else
+      good_input = ["A", "B", "C", "D", "E", "F", "G"]
+      until good_input.any? {|good| good == user_column_choice}
+        puts message.clarify
+        user_column_choice = gets.strip.upcase
+      end
+    end
+    place_user_piece(user_column_choice)
+  end
+
+  def place_user_piece(user_column_choice)
   #     board.receive_user_checker(user_column_choice)
   #     board.update_whether_board_full(board.new_board)
   #     board.display_board
